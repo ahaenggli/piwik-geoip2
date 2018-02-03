1221f12d8c55c3c45e9d5e9c5da4eced22267c00
Adriano Hänggli <adriano_haenggli@hotmail.com>
2018-02-03 19:46:37 +0100

    fix for issue #5 in matomo 3.3.0

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	'fileintegrity.ignore' => DI\add(array(       
+		'misc/GeoIP2-City.mmdb',   
+		'misc/GeoLite2-City.mmdb',   
+		'misc/GeoIP2-Country.mmdb',   
+		'misc/GeoLite2-Country.mmdb',     
+		)),
+);
+?>
